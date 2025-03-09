53a2fb8d8cd3de9c607e4c9299d0722572edd82d
tokima4931 <shiragiku.benizakura@gmail.com>
2025-03-09 14:52:18 +0900

    距離・高度・速度をテキストで表示

--- SevenLanes_unity/Assets/Scripts/CharaMove.cs
+++ SevenLanes_unity/Assets/Scripts/CharaMove.cs
@@ -11,7 +11,7 @@ public class CharaMove : MonoBehaviour, IPlayerMovable
     private float lanesDistance = 2.0f;
     [Header("前進の速度")]
     [SerializeField]
-    private float forwardSpeed = 10.0f;
+    public float forwardSpeed = 10.0f;
 
 
     private void Start()
