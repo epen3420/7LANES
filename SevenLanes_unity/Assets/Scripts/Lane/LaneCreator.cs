f325d85aae377b787930b192bf74213439ec9638
epen3420 <epen3420@gmail.com>
2025-03-13 14:04:33 +0900

    レーンの長さの変更と途切れが見えた時延長可能に

--- SevenLanes_unity/Assets/Scripts/Lane/LaneCreator.cs
+++ SevenLanes_unity/Assets/Scripts/Lane/LaneCreator.cs
@@ -15,22 +15,24 @@ public class LaneCreator : MonoBehaviour
         }
     }
 
-    private const float LANE_DISTANCE = 265.0f;
-    private Vector3 nextLanePos = new Vector3(0, 0, 100);
+    private Vector3 nextLanePos;
     private Quaternion laneRotate = Quaternion.Euler(90, 0, 0);
 
+    [SerializeField]
+    private float laneDistance = 465.0f;
     [SerializeField]
     private GameObject lanePrefab;
     public int LaneCount { get; private set; } = 0;
 
     private void Start()
     {
+        nextLanePos = transform.position;
         LaneCount++;
     }
 
     public void ExpandLane()
     {
-        nextLanePos.z += LANE_DISTANCE;
+        nextLanePos.z += laneDistance;
         // レーン生成は不透明度を上げる感じで出すから修正する
         Instantiate(lanePrefab, nextLanePos, laneRotate);
         Debug.Log("レーンを延長しました");
