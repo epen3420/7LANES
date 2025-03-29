4522ac79c1dc3b907aee98c8255badf571334af2
tokima4931 <shiragiku.benizakura@gmail.com>
2025-03-29 16:25:18 +0900

    レーン数カウントするタイミングを調整。速度の式を修正

--- SevenLanes_unity/Assets/Scripts/Lane/EnterLane.cs
+++ SevenLanes_unity/Assets/Scripts/Lane/EnterLane.cs
@@ -4,8 +4,10 @@ public class EnterLane : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+
         if (!other.CompareTag("Player")) return;
         var laneCount = LaneCreator.instance.LaneCount;
-        other.GetComponent<CharaMove>().forwardSpeed = 10.0f + Mathf.Pow(1.1f, laneCount - 1) + 0.3f * laneCount - 0.3f;
+        other.GetComponent<CharaMove>().forwardSpeed = 10.0f *( Mathf.Pow(1.1f, laneCount) + 0.3f * laneCount);
+        LaneCreator.instance.CountLane();
     }
 }
